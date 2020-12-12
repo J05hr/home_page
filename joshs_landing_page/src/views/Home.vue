307e4eac6775aa414230ac9e7a17429edbae1d52
J05h <J05h@protonmail.com>
2020-12-12 01:06:29 -0500

    framework setup

--- joshs_landing_page/src/views/Home.vue
+++ joshs_landing_page/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="Home hero is-fullh has-background-black-ter">
+    <div class="section is-fullh center">
+      <div class="container column-center">
+        <p class="welcome-text">Welcome, my name is Josh and you've arrived at my landing page.</p>
+        <br/>
+        <p class="welcome-text">This is a central location where you can access a ton of resources.</p>
+      </div>
+      <div class="container center">
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: "Home"
 };
 </script>
